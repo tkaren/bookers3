1750dd431f6c7bdd0e9cf37511751bde738006fb
tsunodakaren <k6432286@gmail.com>
2021-03-02 03:07:52 +0000

    [start] bookers3

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resources :book_comments, only: [:create, :destroy] 
+  end
 end
+# resource :favorites, only: [:create, :destroy]
